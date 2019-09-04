40b262c451a11f9188f16db29221e96a40ed55e1
omergal99 <omergal99gm@gmail.com>
2019-09-04 04:39:52 +0300

    Updates

---
+++ precache-manifest.f933f61a9a8c4d3d18bd225d5d6d4e93.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f6a006fc6c0a96dcf7834619c99a5f",
+    "url": "/my-site/index.html"
+  },
+  {
+    "revision": "46c0310170eb22bc7de6",
+    "url": "/my-site/static/css/main.a60ce95c.chunk.css"
+  },
+  {
+    "revision": "8e8537652089b1bf928f",
+    "url": "/my-site/static/js/2.c0514155.chunk.js"
+  },
+  {
+    "revision": "46c0310170eb22bc7de6",
+    "url": "/my-site/static/js/main.cf6e21cb.chunk.js"
+  },
+  {
+    "revision": "f52ddbeb94c15d5bb818",
+    "url": "/my-site/static/js/runtime~main.96580cd8.js"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/my-site/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/my-site/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/my-site/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "5a3c0694c9b81daf24aff1023a114edb",
+    "url": "/my-site/static/media/SourceSansPro-ExtraLight.5a3c0694.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/my-site/static/media/SourceSansPro-Regular.3f482321.ttf"
+  },
+  {
+    "revision": "2ee0480ac9efb2f09243adf3c85148b3",
+    "url": "/my-site/static/media/header.2ee0480a.png"
+  },
+  {
+    "revision": "712d9273e0e0d3f80d077c60a6a2b557",
+    "url": "/my-site/static/media/hexigon background.712d9273.png"
+  }
+]);
