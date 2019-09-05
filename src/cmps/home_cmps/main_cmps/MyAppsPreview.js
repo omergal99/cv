3ead8956c8e74c487d0c5f99c9400a31fed6eff3
omergal99 <omergal99gm@gmail.com>
2019-09-05 13:21:07 +0300

    first looking good

--- src/cmps/home_cmps/main_cmps/MyAppsPreview.js
+++ src/cmps/home_cmps/main_cmps/MyAppsPreview.js
@@ -42,7 +42,8 @@ function MyAppsPreview({ project }) {
             <p ref={pText}>{project.description}</p>
             <div className="links">
               <a href={project.links.github}><img src="assets/img/icons/github-logo.png" alt="Github" title="Github" /></a>
-              <a href={project.links.website}><img src="assets/img/icons/web.png" alt="Website" title="Website" /></a>
+              <a href={project.links.website}><span>Web</span></a>
+              {/* <a href={project.links.website}><img src="assets/img/icons/webLink.png" alt="Website" title="Website" /></a> */}
             </div>
             <div className="tags">{tags}</div>
           </div>
