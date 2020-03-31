14a23927122c5a5a3cb8a54fd71ecfb895aeb294
omergal99 <omergal99gm@gmail.com>
2020-03-31 05:54:10 +0300

    add hello corona

--- src/cmps/home_cmps/main_cmps/MyApps.js
+++ src/cmps/home_cmps/main_cmps/MyApps.js
@@ -19,15 +19,23 @@ function MyApps({ projects }) {
     ? projects.map(project => <MyAppsPreview project={project} key={project._id} />)
     : empty12.map((empty, idx) => <MyAppsPreview project={emptyProj} key={idx} />);
 
-  // const showProjects = empty12.map((empty, idx) => <MyAppsPreview project={emptyProj} key={idx} />);
-
-  // const showProjects = projects ? projects.map(project => <MyAppsPreview project={project} key={project._id} />)
-  //   : <div style={{ gridColumn: '1/5' }}>'Wait for the Projects..'</div>
+  const corona = {
+    _id: 'corona',
+    name: 'Hello Corona',
+    description: 'Impressive view of the world countries on Map.\nCheck status of your country situation by the Corona virus',
+    desktopImg: 'assets/img/apps/hello corona/desktop.png',
+    mobileImg: 'assets/img/apps/hello corona/mobile.png',
+    links: { github: 'https://github.com/omergal99/hello-corona', website: 'https://omergal99.github.io/hello-corona' },
+    tags: ['React', 'Hooks', 'Redux', 'Responsive', 'SVG-Map', 'Corona virus', 'Heroku API']
+  }
 
   return (
     <div className="projects">
       <h2>My projects</h2>
       <p>All my projects I made. Some with Server and DB, Responsive design.</p>
+      <div className="hello-corona">
+        <MyAppsPreview project={corona} key={corona._id} />
+      </div>
       <ul className="my-apps">
         {showProjects}
       </ul>
